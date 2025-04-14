10dc2cd94781489a39aef55de9155c4cec7a05a1
parvez-tm <parvez.tm@outlook.com>
2025-04-14 14:45:36 +0530

    formarray concept clear

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
+
+interface ITodo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
+  // template: `
+  //   <h1>Hello from PTM!</h1>
+  // `,
+  templateUrl: 'form.html',
+  imports: [CommonModule,ReactiveFormsModule]
 })
 export class App {
-  name = 'Angular';
+  
+  form: FormArray = this.fb.array([]);
+  data: ITodo[] = []; 
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ){
+  }
+
+  // Type-safe getter for form controls
+  // get f(): AbstractControl[] {
+  //   return this.form.controls;
+  // }
+
+  get f(): FormGroup[] {
+    return (this.form.controls as FormGroup[]);
+  }
+
+  ngOnInit(){
+    this.getData()
+  }
+  getData(){
+    this.http.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos').subscribe({
+      next: (data)=>{
+        // this.data = data
+        this.data = data.slice(0, 10);
+        this.buildForm(this.data)
+      }
+    })
+  }
+
+  buildForm(data:ITodo[]){
+    const group = data.map(item => 
+      this.fb.group({
+        completed : [item.completed]
+      })
+    )
+    this.form = this.fb.array(group)
+    
+  }
+
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App,
+  {
+    providers: [
+      provideHttpClient()
+    ]
+  }
+);
